44f872c7e5a44f7ef2147b0995ac699d3b4e7094
marshall8009 <marshall_8009@yahoo.com>
2025-08-16 18:48:52 -0500

    created library project with fodler structure. added data access and data access interface. created save and load data methods

---
+++ HotelAppLibrary/DataAccess/ISqlDataAccess.cs
@@ -0,0 +1,9 @@
+﻿
+namespace HotelAppLibrary.DataAccess
+{
+    public interface ISqlDataAccess
+    {
+        List<T> LoadData<T, U>(string sqlStatement, U parameters, string connectionStringName, bool isStoredProc = false);
+        void SaveData<T>(string sqlStatement, T parameters, string connectionStringName, bool isStoredProc = false);
+    }
+}
