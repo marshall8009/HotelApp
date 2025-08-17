74cb95f910ea62309f3635915b92693413ffc6fe
marshall8009 <marshall_8009@yahoo.com>
2025-08-17 17:55:16 -0500

    database changes, modified bookings table. added a get available rooms SP

---
+++ HotelAppDb/dbo/spRoomTypes_GetAvailableTypes.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [dbo].[spRoomTypes_GetAvailableTypes]
+	@startDate date,
+	@endDate date
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	SELECT rt.Id, rt.Title, rt.Description, rt.Price
+	FROM Rooms r
+	INNER JOIN RoomTypes rt ON rt.Id = r.RoomTypeId
+	WHERE r.IsBooked = 0
+	-- going to try above where clause first as this seems more convoluted than a simple bool
+	--WHERE r.Id not in (
+	--	SELECT b.RoomId
+	--	FROM Bookings b
+	--	WHERE (@startDate < b.StartDate and @endDate > b.EndDate)
+	--		or (b.StartDate <= @endDate and @endDate < b.EndDate)
+	--		or (b.StartDate <= @startDate and @startDate < b.EndDate)
+	--)
+	GROUP BY rt.Id, rt.Title, rt.Description, rt.Price
+END
