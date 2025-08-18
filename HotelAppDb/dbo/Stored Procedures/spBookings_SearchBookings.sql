1aa34e3a66585db94ebff3caa9387aafea5313b4
marshall8009 <marshall_8009@yahoo.com>
2025-08-18 15:45:03 -0500

    created stored proc for booking search

---
+++ HotelAppDb/dbo/Stored Procedures/spBookings_SearchBookings.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[spBookings_SearchBookings]
+	@lastName nvarchar(50),
+	@startDate date
+AS
+BEGIN 
+	SET NOCOUNT ON
+
+	SELECT [b].[Id], [b].[RoomId], [b].[GuestId], [b].[StartDate], [b].[EndDate], 
+		[b].[IsCheckedIn], [b].[TotalPrice], [g].[FirstName], [g].[LastName], 
+		[r].[RoomNumber], [r].[RoomTypeId], [r].[IsBooked], [rt].[Title], 
+		[rt].[Description], [rt].[Price]
+	FROM dbo.Bookings b
+	INNER JOIN dbo.Guests g ON b.GuestId = g.Id
+	INNER JOIN dbo.Rooms r on b.RoomId = r.Id
+	INNER JOIN dbo.RoomTypes rt ON r.RoomTypeId = rt.Id
+	WHERE g.LastName = @lastName AND b.StartDate = @startDate
+END
