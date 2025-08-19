4a6da0749ce3f86f5f88f340935d5a7cc55eda97
marshall8009 <marshall_8009@yahoo.com>
2025-08-19 14:57:59 -0500

    changed stored proc to remove isbooked where clause, solution not practical

--- HotelAppDb/dbo/Stored Procedures/spRoomTypes_GetAvailableTypes.sql
+++ HotelAppDb/dbo/Stored Procedures/spRoomTypes_GetAvailableTypes.sql
@@ -8,14 +8,12 @@ BEGIN
 	SELECT rt.Id, rt.Title, rt.Description, rt.Price
 	FROM dbo.Rooms r
 	INNER JOIN dbo.RoomTypes rt ON rt.Id = r.RoomTypeId
-	WHERE r.IsBooked = 0
-	-- going to try above where clause first as this seems more convoluted than a simple bool
-	--WHERE r.Id not in (
-	--	SELECT b.RoomId
-	--	FROM Bookings b
-	--	WHERE (@startDate < b.StartDate and @endDate > b.EndDate)
-	--		or (b.StartDate <= @endDate and @endDate < b.EndDate)
-	--		or (b.StartDate <= @startDate and @startDate < b.EndDate)
-	--)
+	WHERE r.Id not in (
+		SELECT b.RoomId
+		FROM Bookings b
+		WHERE (@startDate < b.StartDate and @endDate > b.EndDate)
+			or (b.StartDate <= @endDate and @endDate < b.EndDate)
+			or (b.StartDate <= @startDate and @startDate < b.EndDate)
+	)
 	GROUP BY rt.Id, rt.Title, rt.Description, rt.Price
 END
