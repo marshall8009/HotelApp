9e2cf1fca01fbe09966e1260162e4ea8b8dbfe09
marshall8009 <marshall_8009@yahoo.com>
2025-08-19 15:06:07 -0500

    added razor page for room search

---
+++ HotelApp.Web/Pages/RoomSearch.cshtml.cs
@@ -0,0 +1,46 @@
+using HotelAppLibrary.Business;
+using HotelAppLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Identity.Client;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelApp.Web.Pages
+{
+    public class RoomSearchModel : PageModel
+    {
+        private readonly IDatabaseData _db;
+
+        [DataType(DataType.Date)]
+        [BindProperty(SupportsGet = true)]
+        public DateTime StartDate { get; set; } = DateTime.Now;
+
+        [DataType(DataType.Date)]
+        [BindProperty(SupportsGet = true)]
+        public DateTime EndDate { get; set; } = DateTime.Now.AddDays(1);
+
+        [BindProperty(SupportsGet = true)]
+        public bool SearchEnabled { get; set; }
+
+        public List<RoomTypeModel> AvailableRoomTypes { get; set; }
+
+        public RoomSearchModel(IDatabaseData db)
+        {
+            _db = db;
+        }
+
+        public void OnGet()
+        {
+            if (SearchEnabled)
+            {
+                AvailableRoomTypes = _db.GetAvailableRoomTypes(StartDate, EndDate);
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+
+            return RedirectToPage(new { SearchEnabled = true, StartDate, EndDate });
+        }
+    }
+}
